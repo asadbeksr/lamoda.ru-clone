21710258abbad02bdd0eefd58142d09301cb9171
bekdevuz <bazarovrauf@gmail.com>
2021-07-09 20:35:09 +0500

    //disable scroll when opened cart

---
+++ scripts/script.js
@@ -0,0 +1,62 @@
+// location
+const headerCityButton = document.querySelector('.header__city-button');
+
+headerCityButton.textContent = localStorage.getItem('lomoda-location') || ('Ваш город?');
+
+headerCityButton.addEventListener('click', () => {
+    const city = prompt('Укажите ваш город?');
+    headerCityButton.textContent = city;
+    localStorage.setItem('lomoda-location', city);
+});
+
+
+//disable scroll when opened cart
+const disableScroll = () => {
+    const widthScroll = window.innerWidth - document.body.offsetWidth;
+
+    document.body.dbScrollY = window.scrollY;
+
+    document.body.style.cssText = `
+    position: fixed;
+    top: ${-window.scrollY}px;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    padding-right: ${widthScroll}px;
+    `;
+};
+
+const enableScroll = () => {
+    document.body.style.cssText = '';
+    window.scroll({
+        top: document.body.dbScrollY,
+    })
+};
+
+
+
+
+// modal window
+const subheaderCart = document.querySelector('.subheader__cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+
+const cartModalOpen = () => {
+    cartOverlay.classList.add('cart-overlay-open');
+    disableScroll();
+};
+
+const cartModalClose = () => {
+    cartOverlay.classList.remove('cart-overlay-open');
+    enableScroll();
+};
+
+subheaderCart.addEventListener('click', cartModalOpen);
+
+cartOverlay.addEventListener('click', event => {
+    const target = event.target;
+
+    if (target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
+        cartModalClose();
+    }
+});
